83fca452f1fe840721d72dee0763ad9007c9d7a4
Killianoc <Killianoc@Hotmail.com>
2018-03-30 14:22:05 +0100

    Fix accidental typings

--- src/main/java/com/rugby/manager/Application.java
+++ src/main/java/com/rugby/manager/Application.java
@@ -11,7 +11,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;gi
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
